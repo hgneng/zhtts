d6c7c4aa10f5b8d1ceefe8d81b2d44f79b115675
Cameron Wong <hgneng@gmail.com>
2025-06-24 12:44:55 +0800

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+echo '===== setup tsinghua pip repo ====='
+sudo apt install -y python3-pip
+pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+
+echo '===== install pip dependencies ====='
+pip install .
+
+# copy file to /opt/EmotiVoice
+echo '===== install zhttsServer.py ====='
+cp zhttsServer.py /usr/bin/zhttsServer.py
